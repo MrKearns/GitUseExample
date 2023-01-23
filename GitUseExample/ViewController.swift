5b946be320c96116c0075a1b286fa73c5a4597f7
MrKearns <jkearns631@students.mchenry.edu>
2023-01-23 09:31:49 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Jonathan Kearns on 1/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
